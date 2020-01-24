5afaff8da31ae3c0ce1ff7231bca68c7aa52e8db
Yoobin JO <dbqlsdl1234@naver.com>
2020-01-24 22:32:58 +0900

    Image 부분 CornerRadius 정하는 방법 찾음

--- PraticeSwiftUI/PraticeSwiftUI/ItemRow.swift
+++ PraticeSwiftUI/PraticeSwiftUI/ItemRow.swift
@@ -13,9 +13,12 @@ struct ItemRow: View {
     
     var body: some View {
         HStack {
-            Image(item.thumbnailImage)
-            Text(item.name)
-            Text("$\(item.price)")
+            Image(item.thumbnailImage).cornerRadius(10)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+            
         }
     }
 }
