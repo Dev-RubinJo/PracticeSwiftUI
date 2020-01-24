7619a4cc9f91bce2da6546c3e08d9be9a3a9a57c
Yoobin JO <dbqlsdl1234@naver.com>
2020-01-24 22:01:03 +0900

    SwiftUI 공부를 위한 기본 프로젝트 생성

---
+++ SwiftUI/SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI
+//
+//  Created by YooBin Jo on 2020/01/24.
+//  Copyright © 2020 YooBin Jo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
