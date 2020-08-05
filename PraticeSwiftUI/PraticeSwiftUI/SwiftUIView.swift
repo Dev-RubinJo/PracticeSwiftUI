543f6109d0c09f35e393344caf213802213872c1
Rubin.Jo <dbqlsdl1234@naver.com>
2020-08-05 19:49:54 +0900

    초기 View를 SwiftUIView로 변경하고 SwiftUIView 를 계속 커스텀해가며 이것저것 적용중

--- PraticeSwiftUI/PraticeSwiftUI/SwiftUIView.swift
+++ PraticeSwiftUI/PraticeSwiftUI/SwiftUIView.swift
@@ -9,17 +9,58 @@
 import SwiftUI
 
 struct SwiftUIView: View {
+    
+    let carStack: HStack = HStack {
+        Text("Car Image")
+            .foregroundColor(.black)
+        Image(systemName: "car.fill")
+    }
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .font(.largeTitle)
-                .foregroundColor(.red)
+            VStack {
+                Text("Title Message")
+                    .font(.custom("Copperplate", size: 70.0))
+                    .foregroundColor(.black)
+                Text("Text 2")
+                    .padding()
+                    .modifier(StandardTitle())
+                //                    .border(Color.black)
+                //                    .font(.title)
+                //                    .foregroundColor(.black)
+                //                    .shadow(color: .gray, radius: 5, x: 0, y: 10)
+                
+                HStack {
+                    Text("Text 1")
+                        .foregroundColor(.black)
+                    Text("Text 2")
+                        .foregroundColor(.black)
+                }
                 .padding()
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+                
+                
+                MyHStackView()
+                    .font(.largeTitle)
+                self.carStack
+            }
+            Text("Text 5")
+                .font(.headline)
+                .foregroundColor(.black)
+                .onAppear()
+                .onDisappear()
+            Button(action: {print("hello")}) {
+                Image(systemName: "square.and.arrow.down")
             }
         }
+        //        VStack {
+        //            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        //                .font(.largeTitle)
+        //                .foregroundColor(.red)
+        //                .padding()
+        //            Button(action: {}) {
+        //                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+        //            }
+        //        }
     }
 }
 
@@ -30,9 +71,31 @@ struct SwiftUIView_Previews: PreviewProvider {
                 .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
                 .previewDisplayName("iPhone SE")
                 .environment(\.colorScheme, .dark)
-            SwiftUIView()
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-                .previewDisplayName("iPhone 11")
+        }
+        
+    }
+}
+
+struct StandardTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+            .background(Color.white)
+            .border(Color.gray, width: 0.2)
+            .shadow(color: .black, radius: 5, x: 0, y: 5)
+    }
+}
+
+struct MyHStackView: View {
+    var body: some View {
+        HStack {
+            Text("Text 3")
+                .foregroundColor(.blue)
+            Text("Text 4")
+                .foregroundColor(.blue)
+            Image(systemName: "car.fill")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
         }
     }
 }
