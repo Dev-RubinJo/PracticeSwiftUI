3a4dbaec7d0272036e994e73139b55eb84ecc38e
Rubin.Jo <dbqlsdl1234@naver.com>
2020-12-24 23:36:53 +0900

    API 통신 적용 예제(연습중)

---
+++ Movie/Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by YooBin Jo on 2020/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie/Movie/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Movie
+//
+//  Created by YooBin Jo on 2020/12/21.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "2fcf9aa2"
+}
